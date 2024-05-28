db266ef065aa3ee68083bb4f4dc19b8c8bbd5ed4
Charles ugochukwu <Charlkeyz2017@gmail.com>
2024-05-28 12:26:03 +0100

    commit code

--- src/components/TestimonialComponent/Testimonial.jsx
+++ src/components/TestimonialComponent/Testimonial.jsx
@@ -1,27 +1,40 @@
-import React, { useState, useEffect } from "react";
+// import  { useState, useEffect } from "react";
 import style from "../TestimonialComponent/testify.module.css";
 import { testimonailDeatils } from "../../mydataArray/testominalArray";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 const Testimonial = () => {
- const [currentIndex, setCurrentIndex] = useState(0)
-
- function nextSlide(){
-  setCurrentIndex((prev) =>
-  prev === testimonailDeatils.length - 1 ? 0 :  prev + 1)
- }
-//  function prevSlide(){
-//   setCurrentIndex((prev) => 
-//   prev === 0 ? testimonailDeatils.length - 1 : prev - 1 )
-//  }
- function goToNextSlider(index){
-  setCurrentIndex(index)
- }
-
- useEffect(() => {
-  const intervalId = setInterval(nextSlide, 5000); // Auto slide every 5 seconds
-  return () => clearInterval(intervalId); // Clean up on unmount
-}, [currentIndex]);
+ 
+ 
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
 
   
@@ -33,16 +46,18 @@ const Testimonial = () => {
       </div>
       
       
-      <div className={style.testimonaildeatils_flex}>
+      <div className={`${style.testimonaildeatils_flex} sm:w-screen overflow-hidden"`} >
+      <Slider {...settings} className="w-[1350px] sm:w-screen sm:overflow-hidden">
+
       
         {testimonailDeatils.map((items, index) => {
           return (
-                <div key={index} className={style.info_box}>     
-                  <div className={style.persondetails_box}>
+                <div key={index} className={`${style.info_box}`}>     
+                  <div className={`${style.persondetails_box} sm: w-full`}>
                     <div>
-                      <img src={items.img} alt="" />
+                      <img src={items.img} alt="" className="sm:w-[150px]"/>
                     </div>
-                    <div className={style.infoStyle}>
+                    <div className={`${style.infoStyle}`}>
                       <h1>{items.heading}</h1>
                       <p>{items.title}</p>
                     </div>
@@ -54,38 +69,12 @@ const Testimonial = () => {
             
           );
         })}
-      </div>
-      <div className={style.mobileViewTestimonial}>
-          {testimonailDeatils.map((items, index) => {
-          return (
-                <div key={index}  className={ index === currentIndex ? style.info_box : style.hide_box }>     
-                  <div className={style.persondetails_box}>
-                    <div>
-                      <img src={items.img} alt="" />
-                    </div>
-                    <div className={style.infoStyle}>
-                      <h1>{items.heading}</h1>
-                      <p>{items.title}</p>
-                    </div>
-                  </div>
-                  <div className={style.reviews_info}>
-                    <p>{items.paragraph}</p>
-                  </div>
-                </div>
-            
-          );
-        })}
+        </Slider>
 
-          
-          <div className={style.slide_dots}>
-            {testimonailDeatils.map((_, index)=> (
-                <div 
-                  key={index} 
-                  className={index === currentIndex ? `${style.sliderbar} ${style.active}`: style.sliderbar}
-                  onClick={()=> goToNextSlider(index)}></div>
-            ))}
-        </div> 
       </div>
+      
+
+        
       
     </section>
   );
